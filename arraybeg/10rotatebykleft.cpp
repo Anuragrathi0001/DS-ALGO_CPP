5a5b15e727240afe4527a13f79298bcf1e6979ca
Anurag rathi <rathisanjita32@gmail.com>
2025-04-13 19:24:17 +0530

    Array rotation

---
+++ arraybeg/10rotatebykleft.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void rotatebyKleft(int arr[],int num,int k){
+  rotate(arr,arr,arr+k);
+  rotate(arr, arr + k, arr + num);
+  rotate(arr, arr, arr + num);
+}
+int main(){
+    int num,k;
+    cout<<"enter the num";
+    cin >> num;
+    int arr[num];
+    for (int i = 0; i < num; i++)
+    {
+        cout << "enter the element:";
+        cin >> arr[i];
+    }
+    cout << "Enter the num of rotate";
+    cin >> k;
+    k = k % num;
+    rotatebyKleft(arr, num, k);
+    for (int i = 0; i < num; i++)
+    {
+    cout << arr[i];
+    }
+
+    return 0;
+}
