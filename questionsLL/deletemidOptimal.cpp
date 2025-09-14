c3287e4f17fdf0a8b59978b031363d9b88625931
anurag rathi <rathisanjita32@gmail.com>
2025-09-14 23:07:16 +0530

    deletemid2

---
+++ questionsLL/deletemidOptimal.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+    node(int data1,node*next1){
+        data = data1;
+        next = next1;
+    }
+    node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+node* makell(vector<int>arr){
+    node *head = new node(arr[0]);
+    node *mover = head;
+    for (int i = 1; i < arr.size();i++){
+        node *temp = new node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    head->next->next=head->next->next->next->next;
+    return head;
+}
+void printll(node*head){
+    node *temp = head;
+    while(temp){
+        cout << temp->data<<"->";
+        temp = temp->next;
+    }
+    cout << "null";
+}
+node* delmid(node* head) {
+    if (!head || !head->next) {
+        return nullptr; 
+    }
+    node* slow = head;
+    node* fast = head;
+    node* prev = nullptr;
+    while (fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    prev->next = slow->next;
+    delete slow;
+    return head;
+}
+
+
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8};
+    node*head= makell(arr);
+    printll(head);
+    cout << endl;
+    node *deletemid = delmid(head);
+    printll(deletemid);
+    return 0;
+}
