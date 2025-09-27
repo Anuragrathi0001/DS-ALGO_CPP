ad6c156e9f65fd47d7b1a351f7939493415cc13e
anurag rathi <rathisanjita32@gmail.com>
2025-09-27 17:16:28 +0530

    rat in maze updated

--- recursions/mids/ratInaMaze.cpp
+++ recursions/mids/ratInaMaze.cpp
@@ -30,7 +30,11 @@ int main(){
         total(matrix, 0, 0, 4, path, ans, visited);
     }
   
-    
+      for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i];
+        cout << endl;
+    }
     
     return 0;
 }
