b8f23485e4cdcd1ef4ceee2adba66f595d3916dd
anurag rathi <rathisanjita32@gmail.com>
2025-10-01 23:31:30 +0530

    bits manupulation

---
+++ bitManipulation.cpp/032'scomple.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+string Zcomplement(string num){
+    string ans = "";
+    bool spotOne = 0;
+    for (int i = num.size(); i >=0; i++)
+    { 
+      if(num[i]=='1'){
+          spotOne = 1;
+      }
+      if((num[i]=='0'||num[i]=='1')&&spotOne==1){
+      if(num[i]=='0'){
+          ans.push_back('1');
+      }
+      if(num[i]=='1'){
+          ans.push_back('0');
+      }
+      }
+      if((num[i]=='0'||num[i]=='1')&&spotOne==0){
+      if(num[i]=='0'){
+          ans.push_back('0');
+      }
+      if(num[i]=='1'){
+          ans.push_back('1');
+      }
+      }      
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+int main(){
+    string num = "100100";
+ cout<< Zcomplement(num);
+    return 0;
+}
