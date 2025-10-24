bcc0a50a534ce70d01a0b82e440c837536dff879
anurag rathi <rathisanjita32@gmail.com>
2025-10-24 13:20:09 +0530

    stackusingarr

--- stacksAndQueue/stack01.cpp
+++ stacksAndQueue/stack01.cpp
@@ -1,26 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int pop(int arr[],int top){
-      if(top=-1){
-          return -1;
-      }
-      else{
-          arr[top] = 0;
-          top--;
-      }
-}
-void push(int x,int top,int arr[]){
-    if(top=-1){
-        top = 0;
-        arr[top] = x;
+
+int pop(int arr[], int &top) {
+    if (top == -1) {
+        cout << "Stack Underflow" << endl;
+        return -1;
+    } else {
+        int val = arr[top];
+        arr[top] = 0;
+        top--;
+        return val;
     }
-    else{
+}
+
+void push(int x, int arr[], int &top) {
+    if (top == 49) {
+        cout << "Stack Overflow" << endl;
+        return;
+    } else {
         top++;
         arr[top] = x;
     }
 }
-int main(){
+
+int main() {
     int arr[50];
     int top = -1;
-  cout<<  pop(arr, top);
+
+    cout << pop(arr, top) << endl;   // Stack underflow expected
+
+    push(2, arr, top);               // Push value 2
+    cout << pop(arr, top) << endl;   // Pop and print the value
 }
