8c3f7caef234a3465ffac0a173ceeddd757943b8
anurag rathi <rathisanjita32@gmail.com>
2025-10-27 15:36:13 +0530

    prefix to postfix

--- stacksAndQueue/stack11.cpp
+++ stacksAndQueue/stack11.cpp
@@ -1,26 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-string postToPre(string s) {
+string prefixtopostfix(string s){
     stack<string> st;
-
-    for (int i = 0; i < s.size(); i++) {
-        if (isalnum(s[i])) {
+    for (int i = s.size()-1; i>=0;i++){
+        if(isalnum(s[i])){
             st.push(string(1, s[i]));
         }
         else if (s[i] == '-' || s[i] == '+' || s[i] == '*' || s[i] == '/' || s[i] == '^') {
             string sec = st.top(); st.pop();
             string fir = st.top(); st.pop();
-            string ans = string(1, s[i]) + fir + sec;  
+            string ans = fir + sec + string(1, s[i]);
             st.push(ans);
         }
     }
-
     return st.top();
 }
-
-int main() {
-    string s = "AB+DE-F*/";
-    cout << postToPre(s);
+int main(){
+    string s = "/-AB*+DEF";
+ cout<<   prefixtopostfix(s);
     return 0;
 }
