3ff013de1c332241bf9e490079d039b04c178e3e
anurag rathi <rathisanjita32@gmail.com>
2025-11-08 16:08:54 +0530

    maxsumin kelm window

--- 2Pointer_slidingWindow/2p2.cpp
+++ 2Pointer_slidingWindow/2p2.cpp
@@ -1,25 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int maxsuminkthwindow(vector<int>arr,int k){
+
+int maxsuminkthwindow(vector<int> arr, int k){
+    if(k > arr.size()) return -1;
+
     int initialsum = 0;
-    int maxi = 0;
-    for (int i = 0; i < k;i++){
-        initialsum = initialsum + arr[i];
+    for(int i = 0; i < k; i++){
+        initialsum += arr[i];
     }
+
+    int maxi = initialsum;     // important correction
     int l = 0, r = k - 1;
-    while(r<arr.size()-1){
-        initialsum = initialsum - arr[l];
+
+    while(r < arr.size() - 1){
+        initialsum -= arr[l];
         l++;
         r++;
-        initialsum = initialsum+arr[r];
-        maxi = max(maxi, initialsum);
+        initialsum += arr[r];
+        maxi = max(maxi, initialsum); // update after sliding
     }
+
     return maxi;
 }
+
 int main(){
     vector<int> arr = {-1, 2, 3, 3, 4, 5, -1};
     int k = 4;
-    cout<<maxsuminkthwindow(arr,k);
-
+    cout << maxsuminkthwindow(arr, k);
     return 0;
 }
