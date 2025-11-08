c1e9934ceea92b6e1fb4009c1a866519d645e361
anurag rathi <rathisanjita32@gmail.com>
2025-11-08 11:03:51 +0530

    celebrity

---
+++ stacksAndQueue/stack29.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int celebrity(vector<vector<int>>mat){
+    int n = mat.size();
+    vector<int> knowme(n);
+    vector<int> iknow(n);
+    for (int i = 0; i < n;i++){
+        for (int j = 0; j < n;j++){
+            if(mat[i][j]==1){
+                knowme[j]++;
+                iknow[i]++;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if(knowme[i]==n-1&&iknow[i]==0){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    vector<vector<int>> mat = {{0,1,1,0}, {0,0,0,0}, {0,1,0,0}, {1,1,0,0}};
+    cout<<celebrity(mat);
+    return 0;
+}
---
+++ stacksAndQueue/stack30.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int celebrity(vector<vector<int>>mat){
+    int n = mat.size();
+    int low = 0;
+    int high = n - 1;
+    while(low<high){
+        if(mat[low][high]==1){
+            low++;
+        }
+        else if(mat[high][low]==1){
+            high--;
+        }
+        else {
+            low++;
+            high--;
+        }
+           int cand = low;
+
+    // Step 2: Verify candidate
+    for(int i = 0; i < n; i++){
+        if(i != cand){
+            if(mat[cand][i] == 1 || mat[i][cand] == 0) {
+                return -1;
+            }
+        }
+    }
+
+    return cand;
+}
+    }
+
+int main(){
+    vector<vector<int>> mat = {{0,1,1,0}, {0,0,0,0}, {0,1,0,0}, {1,1,0,0}};
+    cout<<celebrity(mat);
+    return 0;
+}
