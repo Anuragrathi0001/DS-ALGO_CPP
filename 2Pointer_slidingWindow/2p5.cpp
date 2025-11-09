2812daf45d6bba7ed4fdffdf84b17ebc52e03bd2
anurag rathi <rathisanjita32@gmail.com>
2025-11-09 22:15:45 +0530

    2papproach

--- 2Pointer_slidingWindow/2p5.cpp
+++ 2Pointer_slidingWindow/2p5.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+int lswsumloretok(vector<int>arr,int k){
+    
+}
 int main(){
-
+    vector<int> arr = {2, 5, 1, 10, 10};
+    int k = 14;
+    lswsumloretok(arr, k);
     return 0;
 }
