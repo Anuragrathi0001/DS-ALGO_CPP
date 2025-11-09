c74ae5ccc3bc67d2c493f1ad457a0af5aa0905fd
anurag rathi <rathisanjita32@gmail.com>
2025-11-09 15:19:36 +0530

    largestsizeofsubarrwithsumk

---
+++ 2Pointer_slidingWindow/2p3.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+//largest size of subarr with sum<=k
+using namespace std;
+int sizeofsubarr(vector<int>arr,int k){
+    int maxlen = 0;
+    for (int i = 0; i < arr.size();i++){
+        int sum = 0;
+        for (int j = i; j < arr.size();j++){
+            sum = sum + arr[j];
+            if(sum<=k){
+                maxlen = max(maxlen, (j - i + 1));
+            }
+            else if(sum>k){ 
+                break;
+            }
+        }
+    }
+    return maxlen;
+}
+int main(){
+    vector<int> arr = {2, 5, 1, 7, 10};
+    int k = 14;
+   cout<< sizeofsubarr(arr, k);
+    return 0;
+}
