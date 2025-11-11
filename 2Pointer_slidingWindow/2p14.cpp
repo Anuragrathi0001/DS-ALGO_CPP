84295f35e19f62129ad42c5cae8f8e1aac5a4151
anurag rathi <rathisanjita32@gmail.com>
2025-11-11 14:16:24 +0530

    maxlensubstrstuff

---
+++ 2Pointer_slidingWindow/2p14.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxlenSubstrwkstuff(string str,int k){
+    int maxlen = 0, l = 0;
+    map<char, int> mpp;
+    for (int r = 0; r < str.length();r++){ 
+        mpp[str[r]]++;
+        while(mpp.size()>k){
+            mpp[str[l]]--; l++;
+            if((mpp[str[l]])==0){ 
+                mpp.erase(str[l]);
+            }
+        }
+        if(mpp.size()<=k){
+            maxlen = max(maxlen, r- l + 1);
+           
+        }
+        
+    }
+    return maxlen;
+}
+int main(){
+    string str = "aaabbccd";
+    int k = 2;
+   cout<< maxlenSubstrwkstuff(str,k);
+    return 0;
+}
