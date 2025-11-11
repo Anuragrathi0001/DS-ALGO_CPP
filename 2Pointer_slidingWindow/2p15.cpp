1f5a540fdbe4871220158212787ddc18c8aa3dc5
anurag rathi <rathisanjita32@gmail.com>
2025-11-11 14:55:19 +0530

    findcountsubstrcontainningthreecharacters

---
+++ 2Pointer_slidingWindow/2p15.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findcountsubstrcontainningthreecharacters(string str){
+      int count = 0;
+    for (int i = 0; i < str.length(); i++) { 
+        vector<int> mpp(3,0); 
+         for (int j = i; j < str.length(); j++)  {
+            mpp[str[j] - 'a']++;
+            if (mpp[0] + mpp[1] + mpp[2] == 3)
+            {
+                count++;
+            }
+     }
+    }
+    return count;
+}
+int main(){
+    string str = "bbacba";
+     cout<<findcountsubstrcontainningthreecharacters(str);
+    return 0;
+}
