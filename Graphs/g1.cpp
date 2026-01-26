56145c94ac47d85c7ecc8329b162fb168a5808b0
anurag rathi <rathisanjita32@gmail.com>
2026-01-26 19:45:01 +0530

    ist code

---
+++ Graphs/g1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Enter number of nodes and edges: ";
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0));
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1; // remove if graph is directed
+    }
+
+    cout << "Adjacency Matrix:\n";
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
