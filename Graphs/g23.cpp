5c787b6e5f770fad9823a84ee8faedf4b81b6703
anurag rathi <rathisanjita32@gmail.com>
2026-02-17 18:26:28 +0530

    fw

--- Graphs/g23.cpp
+++ Graphs/g23.cpp
@@ -21,7 +21,7 @@ void applyFW(vector<vector<int>>&matrix){
     }
     for (int i = 0; i < n;i++){
         for (int j = 0; j < n;j++){
-            if(matrix[i][j]=1e9){
+            if(matrix[i][j]==1e8){
                 matrix[i][j] = -1;
             }
         }
@@ -36,5 +36,11 @@ int main(){
         {-1, -1, -1, 0}
     };
     applyFW(matrix);
-    return 0;
+    for (int i = 0; i < matrix.size();i++){
+        for (int j = 0; j < matrix[0].size();j++){
+            cout << matrix[i][j]<<" ";
+        }
+        cout << endl;
+    }
+        return 0;
 }
