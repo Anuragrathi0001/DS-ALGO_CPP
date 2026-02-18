d2fa51f626a82f378b28eb45cb388cc120d367b6
anurag rathi <rathisanjita32@gmail.com>
2026-02-18 10:50:11 +0530

    prinms

--- Graphs/g24.cpp
+++ Graphs/g24.cpp
@@ -1,6 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-int findwt(vector<vector<int>>)
+int findwt(vector<vector<pair<int,int>>>adj,int n){
+priority_queue<
+    pair<int,int>,
+    vector<pair<int,int>>,
+    greater<pair<int,int>>
+> pq;
+    vector<int> vis(n, 0);
+    pq.push({0, 0});
+    int sum = 0;
+    while(!pq.empty()){
+        auto it = pq.top();
+        pq.pop();
+        int dist = it.first;
+        int node = it.second;
+        if(vis[node]){continue;}
+        vis[node] = 1;
+        sum += dist;
+        for(auto it:adj[node]){
+            int wt = it.second;
+            int adjnode = it.first;
+            if(!vis[adjnode]){
+                pq.push({wt, adjnode});
+            }
+        }
+    }
+    return sum;
+}
 int main(){
     
     int n = 5;
