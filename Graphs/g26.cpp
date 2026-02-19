2aed9dd80bb9c6852f555dda96f5cf743ba02c55
anurag rathi <rathisanjita32@gmail.com>
2026-02-19 17:46:36 +0530

    krshkalStuff

--- Graphs/g26.cpp
+++ Graphs/g26.cpp
@@ -1,5 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
+class disjointSet{
+    vector<int> rank, parent,Size;
+    public:
+    disjointSet(int n){
+        Size.resize(n + 1, 1);
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1, 0);
+        for (int i = 0; i <= n;i++){
+            parent[i] = i;
+        }
+    }
+    int findUPar(int node){
+
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u==ulp_v){
+            return;
+        }
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+void UnionBySize(int u,int v){
+    int ulp_u = findUPar(u);
+    int ulp_v = findUPar(v);
+    if(ulp_u==ulp_v){
+        return;
+    }
+    if(Size[ulp_u]<Size[ulp_v]){
+        parent[ulp_u] = ulp_v;
+        Size[ulp_v] += Size[ulp_u];
+    }
+    else{
+        parent[ulp_v] = ulp_u;
+        Size[ulp_u] += Size[ulp_v];
+    }
+}
+};
 int main(){
 
     return 0;
