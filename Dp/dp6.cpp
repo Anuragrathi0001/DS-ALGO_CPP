eaea04180680a9269b632685d7a31dd6a65b7bd5
anurag rathi <rathisanjita32@gmail.com>
2026-03-10 17:15:30 +0530

    max sum correction

--- Dp/dp6.cpp
+++ Dp/dp6.cpp
@@ -1,19 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int f(int ind,vector<int>arr){
-if(ind==0){
-    return arr[ind];
+
+int f(int ind, vector<int> arr){
+    if(ind == 0){
+        return arr[ind];
+    }
+    if(ind < 0){
+        return 0;
+    }
+
+    int pick = arr[ind] + f(ind - 2, arr);
+    int npick = f(ind - 1, arr);
+
+    return max(pick, npick);
 }
-if(ind<0){
-    return 0;
-}
-int pick = arr[ind] + f(ind - 2,arr);
-int npick = 0 + f(ind,arr);
-return max(pick, npick);
-}
+
 int main(){
-    vector<int>arr = {2, 1, 4, 9};
+    vector<int> arr = {2, 1, 4, 9};
     int ind = arr.size() - 1;
-    cout<<f(ind,arr);
+    cout << f(ind, arr);
     return 0;
 }
